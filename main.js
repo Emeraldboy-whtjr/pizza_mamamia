967f808cb96dfe90ae517f7cfa286de3d1868318
Emeraldboy-whtjr <82824596+Emeraldboy-whtjr@users.noreply.github.com>
2021-07-16 19:50:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-menu_list_array = ["Veg Margherita Pizza","Chicken Tandorri pizza","Veg Supereme pizza","Panner Tikka pizza","Deluxe Veggie pizza","Veg Extravegent pizza"];
-
+menu_list_array = ["Veg Margherita Pizza",
+"Chicken Tandori Pizza",
+"Veg supreme pizza",
+"Panner Tika pizza",
+"Deluxe Veggie pizza",
+"Veg Extravagent pizza"
+                    ];
 
 function getmenu(){
 var htmldata;
-htmldata = "ol< class='menulist >'"
+htmldata="<ol class='menulist'>"
 menu_list_array.sort();
-for(var i=0<menu_list_array;i++){
-    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'; 
 }
-htmldata=htmldata+"</ol>";
-document.getElementById("display_menu").innerHTML=htmldata;
+htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
 }
 
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
 menu_list_array.sort();
-htmldata="<section class='cards'>";
-for(var i=0;i<menu_list_array.length;i++){
-    htmldata=htmldata+'<div calss="card">';
-    +'<img src="images/pizzaImg.png"/>';
+htmldata="<section class='cards'>"
+for(var i=0; i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">'
+    +'<img src="pizzaImg.png"/>'
     +menu_list_array[i] + '</div>';
+
 }
 htmldata=htmldata+"</section>";
-document.getElementById("display_addmenu").innerHTML = htmldata;
-
-
+document.getElementById("display_addedmenu").innerHTML=htmldata;
 }
 
 function add_top(){
-    var item = document.getElementById("add_item").value;
-    menu_list_array.push(item);
-    add_item();
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
 }
